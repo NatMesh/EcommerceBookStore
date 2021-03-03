8b3cf18772cf548bee21eb1741345cc16ba553b1
Nathanael Meshesha <nathanael.meshesha@yahoo.com>
2021-03-02 18:30:10 -0600

    Initial commit

---
+++ MesheshaBooks/MesheshaBooks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MesheshaBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
